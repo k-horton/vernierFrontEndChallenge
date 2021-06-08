655596417c7fdd2cd27ed095ac51177ba037b613
k-horton <khorton17@georgefox.edu>
2021-06-07 21:25:37 -0700

    additional code comments

--- components/chart.js
+++ components/chart.js
@@ -7,12 +7,9 @@ const dataService = new ChallengeDataService;
 export class MyChallengeChart extends LitElement {
   static get properties() {
     return {
-      /**
-       * The chart currently being displayed.
-       */
-      currentData: {type: String},
-      currentChart: {type: String},
-      graphPoints: {type: Array}
+      currentData: {type: String},  // chart data & metadata
+      currentChart: {type: String}, // 'small', 'medium', or 'large'
+      graphPoints: {type: Array}    // array holding the graph points
     };
   }
 
@@ -57,19 +54,37 @@ export class MyChallengeChart extends LitElement {
     this._plotPoints('large');
   }
 
+  /*
+   * Retrieves the data set of the given size, passes the x- and y-column data
+   * to a function that will convert them to the correct form, then calls
+   * the functions that will generate the point table and then update the
+   * page's render.
+   *
+   * @dataSize: a string denoting the data size, either 'small', 'medium',
+   *  or 'large'
+   */
   async _plotPoints(dataSize) {
     this.currentChart = dataSize.toUpperCase();
     this.currentData = await dataService.getDataSet(dataSize);
-    console.log(this.currentData);
     this.graphPoints = this._convertGraphPoints(this.currentData.xColumn.values,
         this.currentData.yColumn.values);
     this._generateTable();
     this.requestUpdate();
   }
 
+  /*
+   * Converts two arrays of x- and y-column data into the form accepted by
+   * challenge-chart, {x: num, y: num}
+   *
+   * @xColumn: array of x-values
+   * @yColumn: array of y-values
+   */
   _convertGraphPoints(xColumn, yColumn) {
     let graphPoints = [];
+
+    // ensure arrays aren't invalid
     if (xColumn && yColumn) {
+      // add the corresponding points to the new array together
       for (let i = 0; i < xColumn.length; i++) {
         graphPoints.push({x: xColumn[i], y: yColumn[i]});
       }
@@ -78,36 +93,41 @@ export class MyChallengeChart extends LitElement {
     return graphPoints;
   }
 
+  /*
+   * Generates a table containing each point of data, where one column is
+   * x-values and the other is y-values.
+   */
   _generateTable() {
     let tableCode = "";
 
     if (this.currentData) {
       let xColVals = this.currentData.xColumn.values;
       let yColVals = this.currentData.yColumn.values;
-      console.log(xColVals);
 
+      // add the column titles
       tableCode +=
         "<table><tr><th>" + this.currentData.xColumn.name +
         "</th><th>" + this.currentData.yColumn.name
         + "</th></tr>";
 
+      // for each point of data, add x and then y to a new row
       for (var i = 0; i < xColVals.length; i++) {
         tableCode +=
           "<tr><td>" + xColVals[i] + "</td><td>"
           + yColVals[i] + "</td></tr>";
       }
 
+      // close the table
       tableCode += "</table>";
 
-      console.log(tableCode);
-
+      // add the full string to the document's HTML
       document.getElementById("table-content").innerHTML = tableCode;
     }
   }
 
   constructor() {
     super();
-    this.currentChart = '';
+    this.currentChart = ' ';
   }
 
   render() {
